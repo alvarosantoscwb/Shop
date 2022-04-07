b34912fd8de7c5ce5588b39a78aade158a99162a
alvaro.santos <alvaro.santos@preambulo.com.br>
2022-04-07 17:17:32 -0300

    d

--- src/components/ShopItem.vue
+++ src/components/ShopItem.vue
@@ -1,14 +1,32 @@
 <template>
   <div
     class="bg-white shadow-2 q-pa-lg"
-    style="text-align: center; width: 400px; flex-wrap: wrap"
+    style="text-align: center; width: 400px"
   >
-    <img style="height: 250px" :src="item.img" />
-    <div>{{ item.nome }}</div>
-    <div class="preco" style="text-decoration: line-through; color: grey">
-      De: R${{ item.preco }}
+    <img style="height: 250px" :src="imgItem" />
+    <div
+      style="
+        text-align: start;
+        font-size: 20px;
+        inline-size: max-content;
+        text-overflow: ellipsis;
+      "
+    >
+      {{ item.nome }}
     </div>
-    <div>Por: R${{ item.descontoPorCento }}</div>
+    <div
+      v-if="precoOriginal"
+      style="text-align: left; text-decoration: line-through; color: grey"
+    >
+      De: R${{ precoOriginal }}
+    </div>
+    <div v-if="precoLabel" style="text-align: left">
+      Preço: R$ {{ precoLabel }}
+    </div>
+    <div v-if="precoCalculado" style="text-align: left">
+      Por: R${{ precoCalculado }}
+    </div>
+    <div style="text-align: left" v-else>Produto indisponível</div>
   </div>
 </template>
 <script>
@@ -19,6 +37,28 @@ export default defineComponent({
   props: {
     item: {
       type: Object,
+    },
+  },
+  computed: {
+    imgItem() {
+      return (
+        this.item.img ??
+        "https://m.media-amazon.com/images/I/41I-kHZbEaL._AC_UL320_.jpg"
+      );
+    },
+    precoLabel() {},
+    precoOriginal() {
+      // se não tiver De: retorna nada
+      return this.item.descontoValor ?? this.item.preco;
+    },
+    // se tiver desconto retorna valor original
+    precoCalculado() {
+      // se tiver desconto por cento calcular porcentagem
+      this.descontoPorCento =
+        Math.round(this.item.descontoPorCento * this.item.preco) / 100;
+      // se tiver descontoValor retorna
+      return this.descontoPorCento ?? this.item.preco;
+      // se não tiver desconto retorna valor original
     },
   },
 });
